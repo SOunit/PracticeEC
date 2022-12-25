9914be62a9f85c00a9f9c6fda49ff0045d8caa92
LAPTOP-5DSTQPA3\workb <sho@synicsw.com>
2022-12-25 12:38:28 -0800

    add bootstrap, atomic design styles

---
+++ PracticeEC/Models/PartialViews/Organisms/UserCardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Entities;
+
+namespace PracticeEC.Models.PartialViews.Organisms
+{
+    public class UserCardViewModel
+    {
+        public User User { get; set; } = new User();
+
+        public string classes { get; set; } = "";
+    }
+}
